106a4476dd94cb3f47aa2bc0579665d9ae88bf02
Gauravotari <97622519+Gauravotari@users.noreply.github.com>
2022-01-12 22:43:24 +0530

    Add files via upload

---
+++ pro2.java
@@ -0,0 +1,13 @@
+class pro2
+{
+       public static void main(String args[])
+       {
+        System.out.println("i am Gaurav Uday Otari from chiplun.i have completed my" 
+                               +"bachelor off engineering in mechanical" 
+                               +"from gharda institute of technology lavel,i also" 
+                               +"done my diploma in mechanical engineering." 
+                               +"from cusrow wadia  institute of technolgy.and also done my highschool study completed" 
+                               +"from paranjape motiwale highshool chiplun");
+       }
+      
+}
